e7a0173d60d13fcd5df5c7b146f699a8ff7ae4c0
Alexander Rowshan <arowshan@usc.edu>
2024-03-02 17:30:28 -0800

    update to galaxy background

--- site/src/pages/LoginPage.jsx
+++ site/src/pages/LoginPage.jsx
@@ -64,43 +64,51 @@ function LoginPage() {
 
 
     return (
-
-        <div className="box">
-            <img src={brainCouchImage} alt="Brain Couch" width="300" height="200"/>
-            <div> {/* Wrapper div for text, optional depending on desired spacing */}
-                <span>B</span>
-                <span>r</span>
-                <span>a</span>
-                <span>i</span>
-                <span>n</span>
-                <span> </span>
-                <span>B</span>
-                <span>e</span>
-                <span>t</span>
+        <>
+            <div className="bg"></div>
+            <div className="star-field">
+                <div className="layer"></div>
+                <div className="layer"></div>
+                <div className="layer"></div>
             </div>
-            <div className="container">
-                <div className="header">
-                    <div className="text">Sign Up</div>
-                    <div className="underline"></div>
+            <div className="box">
+                <img src={brainCouchImage} alt="Brain Couch" width="300" height="200"/>
+                <div> {/* Wrapper div for text, optional depending on desired spacing */}
+                    <span>B</span>
+                    <span>r</span>
+                    <span>a</span>
+                    <span>i</span>
+                    <span>n</span>
+                    <span> </span>
+                    <span>B</span>
+                    <span>e</span>
+                    <span>t</span>
                 </div>
-                <div className="inputs">
-                    <div className="input">
-                        <img src={person} alt="person"/>
-                        <input type="text" name="username" value={formData.username}  onChange={handleChange} placeholder="Username"/>
+                <div className="container">
+                    <div className="header">
+                        <div className="text">Sign Up</div>
+                        <div className="underline"></div>
                     </div>
-                    <div className="input">
-                        <img src={password} alt="password"/>
-                        <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Password"/>
-                    </div>
-                    <div className="submit-container">
-                        <div className="submit" onClick={handleSignUpClick}>Sign Up</div>
-                        <div className="submit" onClick={handleLoginClick}>Login</div>
+                    <div className="inputs">
+                        <div className="input">
+                            <img src={person} alt="person"/>
+                            <input type="text" name="username" value={formData.username} onChange={handleChange}
+                                   placeholder="Username"/>
+                        </div>
+                        <div className="input">
+                            <img src={password} alt="password"/>
+                            <input type="password" name="password" value={formData.password} onChange={handleChange}
+                                   placeholder="Password"/>
+                        </div>
+                        <div className="submit-container">
+                            <div className="submit" onClick={handleSignUpClick}>Sign Up</div>
+                            <div className="submit" onClick={handleLoginClick}>Login</div>
+                        </div>
                     </div>
                 </div>
+
             </div>
-
-        </div>
-
+        </>
 
     );
 }
