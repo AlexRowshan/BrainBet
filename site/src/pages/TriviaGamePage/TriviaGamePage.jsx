1d837a5e2505794bbd6a8533081000adc7d5f72b
Alexander Rowshan <arowshan@usc.edu>
2024-04-05 13:13:56 -0700

    Initial Start game

---
+++ site/src/pages/TriviaGamePage/TriviaGamePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+function TriviaGamePage() {
+    return (
+        <div>
+           Hello
+        </div>
+    );
+}
+
+export default TriviaGamePage;
